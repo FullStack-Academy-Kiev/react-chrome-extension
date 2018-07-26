ed4ebd9f8efd141d8d4e18dbc6702f66c7866e85
Oleksandr Golovatyi <oleksandr.golovatyi@appsflyer.com>
2018-07-26 06:24:04 +0300

    added popup example

---
+++ popup.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function click(e) {
+  chrome.tabs.executeScript(null,
+      {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
+  window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
+});
