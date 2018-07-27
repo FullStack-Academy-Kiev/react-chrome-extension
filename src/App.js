5c2abf0fd710382c8210c66cf5a7b15720e0eca9
Oleksandr Golovatyi <golovatyi@hotmail.com>
2018-07-27 15:25:38 +0300

    added react app in content script

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  changeColor(e) {
-    window.chrome.tabs.executeScript(null, { code: "document.body.style.backgroundColor='" + e.target.id + "'" });
-  }
-
   render() {
     return (
-      <div className="App" style={{ minWidth: '400px' }}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Chrome Extension</h1>
-        </header>
-        <div className="App-intro">
-          <div className="item" id="red" onClick={this.changeColor.bind(this)}>red</div>
-          <div className="item" id="blue" onClick={this.changeColor.bind(this)}>blue</div>
-          <div className="item" id="green" onClick={this.changeColor.bind(this)}>green</div>
-          <div className="item" id="yellow" onClick={this.changeColor.bind(this)}>yellow</div>
+      <div className="App">
+        <div className="App-header">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png" className="App-logo" alt="logo" />
+          <h2>Welcome to React App inside Web Page</h2>
         </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
