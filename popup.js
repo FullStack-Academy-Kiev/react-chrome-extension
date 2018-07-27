88103828a0199364e28ad32a779bb32bcb02089c
Oleksandr Golovatyi <golovatyi@hotmail.com>
2018-07-27 11:40:23 +0300

    added content settings example

--- popup.js
+++ popup.js
@@ -1,14 +1,46 @@
-'use strict';
+var incognito;
+var url;
 
-function click(e) {
-  chrome.tabs.executeScript(null,
-      {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
-  window.close();
+function settingChanged() {
+  var type = this.id;
+  var setting = this.value;
+  var pattern = /^file:/.test(url) ? url : url.replace(/\/[^\/]*?$/, '/*');
+  console.log(type+' setting for '+pattern+': '+setting);
+  // HACK: [type] is not recognised by the docserver's sample crawler, so
+  // mention an explicit
+  // type: chrome.contentSettings.cookies.set - See http://crbug.com/299634
+  chrome.contentSettings[type].set({
+        'primaryPattern': pattern,
+        'setting': setting,
+        'scope': (incognito ? 'incognito_session_only' : 'regular')
+      });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var current = tabs[0];
+    incognito = current.incognito;
+    url = current.url;
+    var types = ['cookies', 'images', 'javascript', 'location', 'plugins',
+                 'popups', 'notifications', 'microphone', 'camera',
+                 'unsandboxedPlugins', 'automaticDownloads'];
+    types.forEach(function(type) {
+      // HACK: [type] is not recognised by the docserver's sample crawler, so
+      // mention an explicit
+      // type: chrome.contentSettings.cookies.get - See http://crbug.com/299634
+      chrome.contentSettings[type] && chrome.contentSettings[type].get({
+            'primaryUrl': url,
+            'incognito': incognito
+          },
+          function(details) {
+            document.getElementById(type).disabled = false;
+            document.getElementById(type).value = details.setting;
+          });
+    });
+  });
+
+  var selects = document.querySelectorAll('select');
+  for (var i = 0; i < selects.length; i++) {
+    selects[i].addEventListener('change', settingChanged);
   }
 });
